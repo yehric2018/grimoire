2eda2a9ba367b78f366d185a7d0a397c14b38a00
Kent Zeng <kentzeng@uw.edu>
2021-03-21 17:43:08 -0700

    testing index.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class App extends Component<{}, {}> {
+
+    render() {
+        return (
+            <p>testing</p>
+        );
+    }
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>testing</p>, document.getElementById('root'));
